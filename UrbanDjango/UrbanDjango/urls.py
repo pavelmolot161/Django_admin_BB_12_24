6cc0396c6603196855f166a5bdae649c39ab12f5
pavelmolot161 <pavel.molot.161@yandex.ru>
2024-12-29 14:21:30 +0300

    module 19 DZ 3 на ветке [M_19_homework_3] середина

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -14,21 +14,59 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include                             ### - (+)
-
 ### - 15.12.24
 ### - 15.12.24 (+)
 ### - 20.12.24 (++)
 ### - 22.12.24 (+++)
-
+### - 22.12.24 (++++)
+from django.contrib import admin
+from django.urls import path, include                              ### - (+)
 # from task2.views import func_temp, class_temp
-#from task3.views import platform_task, games_task, cart_task   ### - 18.12.24 task3 переехал в task4
-from task5.views import sign_up_by_django, sign_up_by_html
+#from task3.views import platform_task, games_task, cart_task      ### - 18.12.24 task3 переехал в task4
+from task1.views import sign_up_by_django, sign_up_by_html                  ### - (++++)
 from django.views.generic import TemplateView
+from task1.views import platform_task, games_task, cart_task, menu_task     ### - (++++)                                      ### - (++++)
+from task1 import views
+
+app_name = 'task1'                                                 ### - (++++)
 
 urlpatterns = [
-    path('', sign_up_by_django),                                   ### - (++)
-    path('django_sign_up/', sign_up_by_html),                      ### - (++)
-    path('admin/', admin.site.urls),                               ### - (+)
+    path('', views.sign_up_by_django, name='sign_up'),
+    path('django_sign_up/', views.sign_up_by_html, name='sign_up_html'),
+    path('admin/', admin.site.urls),
+    path('platform', platform_task, name='platform_task'),
+    path('menu', menu_task, name='menu_task'),
+    path('games', games_task, name='games_task'),
+    path('cart', cart_task, name='cart_task'),
 ]
+
+
+
+
+# urlpatterns = [
+#     path('', sign_up_by_django),                                   ### - (++)
+#     path('django_sign_up/', sign_up_by_html),                      ### - (++)
+#     path('admin/', admin.site.urls),                               ### - (+)
+#     path('menu_task/', menu_task, name="menu"),                               ### - (++++)
+#     path('platform_task/', platform_task, name="platform"),                       ### - (++++)
+#     path('games_task/',games_task, name="games"),                              ### - (++++)
+#     path('cart_task/',cart_task, name="cart"),                                ### - (++++)
+# ]
+
+
+
+###______________________________________________________________________________________________________
+
+# from django.contrib import admin
+# from django.urls import path
+#
+# ### - 18.12.24 (+)
+# from task4 import views
+#
+# urlpatterns = [
+#
+#     path('menu_task/', views.menu_task),                               ### - (+)
+#     path('platform_task/', views.platform_task),                       ### - (+)
+#     path('games_task/',views.games_task),                              ### - (+)
+#     path('cart_task/',views.cart_task),                                ### - (+)
+# ]
--- UrbanDjango/task1/views.py
+++ UrbanDjango/task1/views.py
@@ -1,3 +1,97 @@
-from django.shortcuts import render
 
-# Create your views here.
+### - 29.12.24 (+)
+
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django import forms
+
+context = {                                                                 ### - (+)
+        'games': ["Atomic Heart", "Cyberpunk 2077", "PayDay 2"]
+    }
+def menu_task(request):
+    return render(request, "fourth_task/menu.html")
+
+def platform_task(request):
+    return render(request, "fourth_task/platform.html")
+
+def games_task(request):
+    return render(request, "fourth_task/games.html", context)
+
+def cart_task(request):
+    return render(request, "fourth_task/cart.html")
+
+###____________________________________________________________________________________________________
+# Псевдо-список существующих пользователей
+existing_users = ['user1', 'user2', 'user3']
+
+# Форма для регистрации
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=30, label='Введите логин')
+    password = forms.CharField(widget=forms.PasswordInput, min_length=8, label='Введите пароль')
+    repeat_password = forms.CharField(widget=forms.PasswordInput, min_length=8, label='Повторите пароль')
+    age = forms.CharField(max_length=3, label='Введите свой возраст')
+
+def sign_up_by_django(request):
+    info = {}                                                                     # Пустой словарь для контекста
+    error = None                                                                  # Переменная для хранения ошибок
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            # Обработка данных из POST запроса
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = int(form.cleaned_data['age'])                                   # Преобразуем возраст в целое число
+
+            # Проверка существования пользователя
+            if username in existing_users:
+                error = "Пользователь уже существует."
+                form.add_error('username', error)                            # Добавляем ошибку к полю логина
+            # Проверка возраста
+            elif age < 18:
+                error = "Вы должны быть старше 18."
+                form.add_error('age', error)                                 # Добавляем ошибку к полю возраста
+            # Проверка совпадения паролей
+            elif password != repeat_password:
+                error = "Пароли не совпадают."                            # Добавляем ошибку к полю повторного пароля
+                form.add_error('repeat_password', error)
+            else:
+                # Здесь можно добавить логику для сохранения пользователя
+                existing_users.append(username)  # Добавляем пользователя в список
+                return HttpResponse(f"Приветствуем, {username}!")
+
+    else:
+        form = SignUpForm()
+
+    info['error'] = error                                                         # Добавление ошибки в словарь info
+    return render(request, 'registration_page.html', {'info': info, 'form': form, 'existing_users': existing_users})
+
+def sign_up_by_html(request):
+    info = {}                                                                     # Пустой словарь для контекста
+    error = None                                                                  # Переменная для хранения ошибок
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = request.POST.get('age')
+
+        # Проверка существования пользователя
+        if username in existing_users:
+            error = "Пользователь уже существует."
+
+        # Проверка возраста
+        elif int(age) < 18:
+            error = "Вы должны быть старше 18."
+
+        # Проверка совпадения паролей
+        elif password != repeat_password:
+            error = "Пароли не совпадают."                                         # Добавляем ошибку в словарь
+
+        else:
+            # Здесь можно добавить логику для проверки и сохранения пользователя
+            existing_users.append(username)                                        # Добавляем пользователя в список
+            return HttpResponse(f"Приветствуем, {username}!")
+
+    info['error'] = error  # Добавление ошибки в словарь info
+    return render(request, 'registration_page.html', {'info': info, 'existing_users': existing_users})
